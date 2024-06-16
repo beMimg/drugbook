75ab3bdcd9a14e72dfff22ec20c4d6161733eddb
beMimg <bernardommguerreiro@gmail.com>
2024-06-16 11:19:17 +0200

    Basic route setup

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Nav from "./Nav";
+
+const Layout = () => {
+  return (
+    <Box display="flex" flexDirection="column" minHeight="100vh">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            BOOKDRUG
+          </Typography>
+          <Nav />
+        </Toolbar>
+      </AppBar>
+      <Container component="main" sx={{ flexGrow: 1, py: 3 }}>
+        <Outlet />
+      </Container>
+      <Footer />
+    </Box>
+  );
+};
+
+export default Layout;
---
+++ src/pages/DrugInfo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DrugInfo = () => {
+  return <div>DrugInfo</div>;
+};
+
+export default DrugInfo;
