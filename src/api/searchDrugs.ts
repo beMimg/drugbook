439f61d673fa3804f39fe95d79bf431977ad1c4c
beMimg <bernardommguerreiro@gmail.com>
2024-06-17 12:41:17 +0200

    Add functionality to Autocomplete, fetches data, and navigates when option selected

--- src/api/searchDrugs.ts
+++ src/api/searchDrugs.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const searchDrug = async (query: string) => {
   try {
     const response = await axios.get(
-      `https://api.fda.gov/drug/label.json?search=openfda.generic_name:*${query}*&limit=3`
+      `https://api.fda.gov/drug/label.json?count=openfda.generic_name.exact&search=openfda.generic_name:*${query}*&limit=100`
     );
     console.log(response.data);
     return response.data.results;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,43 +2,61 @@ import { Autocomplete, TextField } from "@mui/material";
 import { useState, useCallback } from "react";
 import { searchDrug } from "../api/searchDrugs";
 import debounce from "lodash/debounce";
+import { useNavigate } from "react-router-dom";
+
+interface Drug {
+  term: string;
+}
 
 const SearchBar = () => {
-  const [query, setQuery] = useState("");
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState("");
+  const [options, setOptions] = useState<string[]>([]);
 
-  const fetchSuggestions = async (value: string) => {
-    if (value.length > 0) {
-      const drugs = await searchDrug(value);
-      const genericNames = drugs.map(
-        (drug: any) => drug.openfda.generic_name[0]
-      );
-      setSuggestions(genericNames);
+  // Used to nagivate thro routers, since we can't directly use Link in the Autocomplete component.
+  const navigate = useNavigate();
+
+  // value as a string because the input of autocomplete will always be a string.
+  const fetchOptions = async (value: string) => {
+    // Reduce fetching by requiring more than one character.
+    if (value.length > 1) {
+      const drugsData = await searchDrug(value);
+      // lets map through the drugs data, only if it exists .
+      if (drugsData) {
+        const genericNameData = drugsData.map((drug: Drug) => drug.term); // get the generic name on proprety term, must be a STRING.
+        setOptions(genericNameData);
+      }
     } else {
-      setSuggestions([]);
+      setOptions([]);
     }
   };
 
-  const debouncedFetchSuggestions = useCallback(
-    debounce(fetchSuggestions, 300),
-    []
-  );
+  // Debounce for fetching, reducing API calls.
+  const debounceFetchOptions = useCallback(debounce(fetchOptions, 300), []);
 
-  const handleInputChange = (event: any, value: string) => {
-    setInputValue(value);
-    debouncedFetchSuggestions(value);
+  // When one of the options is selected calls this function.
+  const handleOptionChange = (event: any) => {
+    const { value } = event.target;
+    if (value) {
+      // Use the value (Selected Option), to create a path to the route.
+      const path = `/drug/${value}`;
+      navigate(path);
+    }
+  };
+
+  // When the input is changed call this function.
+  const handleInputChange = (event: any) => {
+    const { value } = event.target;
+    debounceFetchOptions(value); // Debounce for fetching, reducing API calls.
   };
 
   return (
     <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={suggestions}
-      inputValue={inputValue}
+      options={options}
+      onChange={handleOptionChange}
       onInputChange={handleInputChange}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Drug" />}
+      sx={{ width: "300px" }}
+      renderInput={(params) => (
+        <TextField {...params} label="Search" variant="outlined" />
+      )}
     />
   );
 };
