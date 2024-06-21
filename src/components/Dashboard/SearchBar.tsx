02e94e2d2afc17da732922c5b0b2c7db6c6e6dc3
beMimg <bernardommguerreiro@gmail.com>
2024-06-21 11:48:41 +0200

    Refator SearchBar for production, ESLINT was causing issues

--- src/components/Dashboard/SearchBar.tsx
+++ src/components/Dashboard/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { Autocomplete, CircularProgress, TextField } from "@mui/material";
-import { useState, useCallback } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { searchDrug } from "../../api/searchDrugs";
 import debounce from "lodash/debounce";
 import { useNavigate } from "react-router-dom";
@@ -11,44 +11,44 @@ interface Drug {
 const SearchBar = () => {
   const [options, setOptions] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
-  // Used to nagivate thro routers, since we can't directly use Link in the Autocomplete component.
   const navigate = useNavigate();
 
-  // value as a string because the input of autocomplete will always be a string.
   const fetchOptions = async (value: string) => {
-    // Reduce fetching by requiring more than one character.
     try {
       if (value.length > 1) {
         setLoading(true);
         const drugsData = await searchDrug(value);
-        // lets map through the drugs data, only if it exists .
         if (drugsData) {
-          const genericNameData = drugsData.map((drug: Drug) => drug.term); // get the generic name on proprety term, must be a STRING.
+          const genericNameData = drugsData.map((drug: Drug) => drug.term);
           setOptions(genericNameData);
         }
       }
     } catch (err) {
+      console.error(err);
     } finally {
       setLoading(false);
     }
   };
 
-  // Debounce for fetching, reducing API calls.
-  const debounceFetchOptions = useCallback(debounce(fetchOptions, 300), []);
+  // Create a debounced version of fetchOptions
+  const debounceFetchOptions = useMemo(() => debounce(fetchOptions, 300), []);
 
-  // When one of the options is selected calls this function.
+  useEffect(() => {
+    // Cleanup the debounced function on component unmount
+    return () => {
+      debounceFetchOptions.cancel();
+    };
+  }, [debounceFetchOptions]);
+
   const handleOptionChange = (event: any, value: string | null) => {
     if (value) {
-      // Use the value (Selected Option), to create a path to the route.
       const path = `/list/${value}?page=1`;
       navigate(path);
     }
   };
 
-  // When the input is changed call this function.
-  const handleInputChange = (event: any) => {
-    const { value } = event.target;
-    debounceFetchOptions(value); // Debounce for fetching, reducing API calls.
+  const handleInputChange = (event: any, value: string) => {
+    debounceFetchOptions(value);
   };
 
   return (
