b0b5c022c63a83af13bd8aa5c58fdc923de7760f
beMimg <bernardommguerreiro@gmail.com>
2024-06-21 11:48:55 +0200

    remove unused imports

--- src/components/Dashboard/Hero.tsx
+++ src/components/Dashboard/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import SearchBar from "./SearchBar";
 import medicineSvg from "../../assets/images/medicine.svg";
 
